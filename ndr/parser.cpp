763767bab0b44dbd29027b2470baf3102fd31882
Pal Mezei <sirpalee@gmail.com>
2019-11-22 17:59:07 +1100

    Supporting Ndr changes between 0.19.11 and 0.20.2.

--- ndr/parser.cpp
+++ ndr/parser.cpp
@@ -44,6 +44,7 @@
 #include <pxr/usd/usd/attribute.h>
 #include <pxr/usd/usd/property.h>
 
+#include "ndrarnold.h"
 #include "utils.h"
 
 PXR_NAMESPACE_OPEN_SCOPE
@@ -128,7 +129,9 @@ NdrNodeUniquePtr NdrArnoldParserPlugin::Parse(const NdrNodeDiscoveryResult& disc
         discoveryResult.discoveryType, // context
         discoveryResult.sourceType,    // sourceType
         discoveryResult.uri,           // uri
+#ifdef USD_HAS_NEW_SDR_NODE_CONSTRUCTOR
         discoveryResult.uri,           // resolvedUri
+#endif
         std::move(properties)));
 }
 
