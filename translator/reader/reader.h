c4b43b3d9e487fd0b69c16744ca173035dcdd124
Pal Mezei <sirpalee@users.noreply.github.com>
2020-02-04 15:47:05 +1100

    Update reader.h
    Formatting fixes.

--- translator/reader/reader.h
+++ translator/reader/reader.h
@@ -79,10 +79,10 @@ public:
     AtNode *getDefaultShader();
     AtNode *lookupNode(const char *name) {
         AtNode *node = AiNodeLookUpByName(_universe, name, _procParent);
-        // We don't want to take into account nodes that were created by a parent procedural 
+        // We don't want to take into account nodes that were created by a parent procedural
         // (see #172). It happens that calling AiNodeGetParent on a child node that was just
         // created by this procedural returns nullptr. I guess we'll get a correct result only
-        // after the procedural initialization is finished. The best test we can do now is to 
+        // after the procedural initialization is finished. The best test we can do now is to
         // ignore the node returned by AiNodeLookupByName if it has a non-null parent that
         // is different from the current procedural parent
         if (node) {
