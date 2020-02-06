c15d45947a7ff7f9c853fc5dcd4338e8bca0515a
Pal Mezei <sirpalee@gmail.com>
2020-02-06 14:00:43 +1100

    Adding an option to include headers in targets.

--- cmake/utils/options.cmake
+++ cmake/utils/options.cmake
@@ -17,6 +17,7 @@ option(USD_STATIC_BUILD "USD is built as a static, monolithic library." OFF)
 option(TBB_STATIC_BUILD "TBB is built as a static library." OFF)
 option(BUILD_USE_CUSTOM_BOOST "Using a custom boost layout." OFF)
 option(BUILD_DISABLE_CXX11_ABI "Disable the use of the new CXX11 ABI" OFF)
+option(BUILD_HEADERS_AS_SOURCES "Add headers are source files to the target to help when generating IDE projects." OFF)
 
 option(BUILD_SCHEMAS "Builds the USD Schemas" ON)
 option(BUILD_RENDER_DELEGATE "Builds the Render Delegate" ON)
