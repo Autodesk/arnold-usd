7b6472f7cb385025aa277a9d92baa898262eb6cd
Pal Mezei <sirpalee@gmail.com>
2020-03-04 22:01:46 +1100

    Using the new instancer procedural. Fixes #256

--- render_delegate/shape.cpp
+++ render_delegate/shape.cpp
@@ -31,9 +31,15 @@ HdArnoldShape::HdArnoldShape(
 HdArnoldShape::~HdArnoldShape()
 {
     AiNodeDestroy(_shape);
+#ifdef HDARNOLD_USE_INSTANCER
+    if (_instancer != nullptr) {
+        AiNodeDestroy (_instancer);
+    }
+#else
     for (auto* instance : _instances) {
         AiNodeDestroy(instance);
     }
+#endif
 }
 
 void HdArnoldShape::Sync(
@@ -50,7 +56,11 @@ void HdArnoldShape::SetVisibility(uint8_t visibility)
 {
     // Either the shape is not instanced or the instances are not yet created. In either case we can set the visibility
     // on the shape.
+#ifdef HDARNOLD_USE_INSTANCER
+    if (_instancer == nullptr) {
+#else
     if (_instances.empty()) {
+#endif
         AiNodeSetByte(_shape, str::visibility, visibility);
     }
     _visibility = visibility;
@@ -80,21 +90,52 @@ void HdArnoldShape::_SyncInstances(
     if (!HdChangeTracker::IsInstancerDirty(dirtyBits, id) && !HdChangeTracker::IsInstanceIndexDirty(dirtyBits, id) &&
         !force) {
         // Visibility still could have changed outside the shape.
+#ifdef HDARNOLD_USE_INSTANCER
+        _UpdateInstanceVisibility(1, param);
+#else
         _UpdateInstanceVisibility(_instances.size(), param);
+#endif
         return;
     }
     param->End();
     // We need to hide the source mesh.
     AiNodeSetByte(_shape, str::visibility, 0);
+#ifndef HDARNOLD_USE_INSTANCER
 #if 1 // Forcing the re-creation of instances.
     for (auto* instance : _instances) {
         AiNodeDestroy(instance);
     }
     _instances.clear();
 #endif
+#endif
     auto& renderIndex = sceneDelegate->GetRenderIndex();
     auto* instancer = static_cast<HdArnoldInstancer*>(renderIndex.GetInstancer(instancerId));
     const auto instanceMatrices = instancer->CalculateInstanceMatrices(id);
+#ifdef HDARNOLD_USE_INSTANCER
+    if (_instancer == nullptr) {
+        _instancer = AiNode(_delegate->GetUniverse(), str::instancer);
+        std::stringstream ss;
+        ss << AiNodeGetName(_shape) << "_instancer";
+        AiNodeSetStr(_instancer, str::name, ss.str().c_str());
+        AiNodeSetPtr(_instancer, str::nodes, _shape);
+        AiNodeDeclare(_instancer, str::instance_inherit_xform, "constant array BOOL");
+        AiNodeSetArray(_instancer, str::instance_inherit_xform, AiArray(1, 1, AI_TYPE_BOOLEAN, true));
+    }
+    const auto instanceCount = static_cast<uint32_t>(instanceMatrices.size());
+    auto* matrixArray = AiArrayAllocate(instanceCount, 1, AI_TYPE_MATRIX);
+    auto* nodeIdxsArray = AiArrayAllocate(instanceCount, 1, AI_TYPE_UINT);
+    auto* matrices = static_cast<AtMatrix*>(AiArrayMap(matrixArray));
+    auto* nodeIdxs = static_cast<uint32_t*>(AiArrayMap(nodeIdxsArray));
+    for (auto i = decltype(instanceCount){0}; i < instanceCount; i += 1) {
+        matrices[i] = HdArnoldConvertMatrix(instanceMatrices[i]);
+        nodeIdxs[i] = 0;
+    }
+    AiArrayUnmap(matrixArray);
+    AiArrayUnmap(nodeIdxsArray);
+    AiNodeSetArray(_instancer, str::instance_matrix, matrixArray);
+    AiNodeSetArray(_instancer, str::node_idxs, nodeIdxsArray);
+    AiNodeSetArray(_instancer, str::instance_visibility, AiArray(1, 1, AI_TYPE_BYTE, _visibility));
+#else
     const auto oldSize = _instances.size();
     const auto newSize = instanceMatrices.size();
     for (auto i = newSize; i < oldSize; ++i) {
@@ -111,16 +152,30 @@ void HdArnoldShape::_SyncInstances(
         ss << AiNodeGetName(_shape) << "_instance_" << i;
         AiNodeSetStr(instance, str::name, ss.str().c_str());
     }
-
     _UpdateInstanceVisibility(oldSize);
-
     for (auto i = decltype(newSize){0}; i < newSize; ++i) {
         AiNodeSetMatrix(_instances[i], str::matrix, HdArnoldConvertMatrix(instanceMatrices[i]));
     }
+#endif
 }
 
 void HdArnoldShape::_UpdateInstanceVisibility(size_t count, HdArnoldRenderParam* param)
 {
+#ifdef HDARNOLD_USE_INSTANCER
+    if (_instancer == nullptr) {
+        return;
+    }
+    auto* instanceVisibility = AiNodeGetArray(_instancer, str::instance_visibility);
+    const auto currentVisibility = (instanceVisibility != nullptr && AiArrayGetNumElements(instanceVisibility) == 1) ?
+        AiArrayGetByte(instanceVisibility, 0) : ~_visibility;
+    if (currentVisibility == _visibility) {
+        return;
+    }
+    if (param != nullptr) {
+        param->End();
+    }
+    AiNodeSetArray(_instancer, str::instance_visibility, AiArray(1, 1, AI_TYPE_BYTE, _visibility));
+#else
     if (count == 0 || _instances.empty()) {
         return;
     }
@@ -139,6 +194,7 @@ void HdArnoldShape::_UpdateInstanceVisibility(size_t count, HdArnoldRenderParam*
     for (auto index = decltype(count){0}; index < count; index += 1) {
         AiNodeSetByte(_instances[index], str::visibility, _visibility);
     }
+#endif
 }
 
 PXR_NAMESPACE_CLOSE_SCOPE
