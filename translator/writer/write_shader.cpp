059114e02cbe2dd7757acab4a689ff1b23499eaf
Pal Mezei <sirpalee@gmail.com>
2020-04-16 18:22:22 +1000

    Formatting changes.

--- translator/writer/write_shader.cpp
+++ translator/writer/write_shader.cpp
@@ -37,43 +37,43 @@ PXR_NAMESPACE_USING_DIRECTIVE
  *"info:id" attribute. Input parameter are saved in the "input:" namespace.
  **/
 
+namespace {
+
+inline void SplitString(const std::string &input, std::vector<std::string> &result)
+{
+    std::string::size_type start = 0;
+    while (true) {
+        // delimiters: semicolon and space
+        std::string::size_type semicolon = input.find(";", start);
+        std::string::size_type space = input.find(" ", start);
+        std::string::size_type end = std::min(semicolon, space);
+        bool notFound = (end == std::string::npos);
+        std::string::size_type width = (notFound ? input.length() : end) - start;
+        std::string name = input.substr(start, width);
+        if (!name.empty())
+            result.push_back(name);
+        if (notFound)
+            break;
+        start = end + 1;
+    }
+}
+
+}
+
 void UsdArnoldWriteShader::Write(const AtNode *node, UsdArnoldWriter &writer)
 {
-    UsdShadeShader shaderAPI = UsdShadeShader::Define(writer.GetUsdStage(), 
-                                          SdfPath(GetArnoldNodeName(node)));
+    UsdShadeShader shaderAPI = UsdShadeShader::Define(writer.GetUsdStage(), SdfPath(GetArnoldNodeName(node)));
     // set the info:id parameter to the actual shader name
-    shaderAPI.CreateIdAttr().Set(TfToken(_usdShaderId)); 
+    shaderAPI.CreateIdAttr().Set(TfToken(_usdShaderId));
     UsdPrim prim = shaderAPI.GetPrim();
     _WriteArnoldParameters(node, writer, prim, "inputs");
 }
 
-static inline void SplitString(const std::string& input, std::vector<std::string> &result)
-{
-   std::string::size_type start = 0;
-   while (1)
-   {
-      // delimiters: semicolon and space
-      std::string::size_type semicolon = input.find(";", start);
-      std::string::size_type space = input.find(" ", start);
-      std::string::size_type end = std::min(semicolon, space);
-      bool notFound = (end == std::string::npos);
-      std::string::size_type width = (notFound ? input.length() : end) - start;
-      std::string name = input.substr(start, width);
-      if (!name.empty())
-         result.push_back(name);
-      if (notFound)
-         break;
-      start = end + 1;
-   }
-}
-
-
 void UsdArnoldWriteToon::Write(const AtNode *node, UsdArnoldWriter &writer)
 {
-    UsdShadeShader shaderAPI = UsdShadeShader::Define(writer.GetUsdStage(), 
-                                          SdfPath(GetArnoldNodeName(node)));
+    UsdShadeShader shaderAPI = UsdShadeShader::Define(writer.GetUsdStage(), SdfPath(GetArnoldNodeName(node)));
     // set the info:id parameter to the actual shader name
-    shaderAPI.CreateIdAttr().Set(TfToken(_usdShaderId)); 
+    shaderAPI.CreateIdAttr().Set(TfToken(_usdShaderId));
     UsdPrim prim = shaderAPI.GetPrim();
     _WriteArnoldParameters(node, writer, prim, "inputs");
 
@@ -83,18 +83,17 @@ void UsdArnoldWriteToon::Write(const AtNode *node, UsdArnoldWriter &writer)
     // pointing at a light name in the scene. Since we convert the node names
     // when writing to usd, these strings aren't matching the usd light names
     AtString rimLight = AiNodeGetStr(node, "rim_light");
-    AtNode *rimLightNode = rimLight.empty() ? nullptr : 
-                    AiNodeLookUpByName(universe, rimLight.c_str());
+    AtNode *rimLightNode = rimLight.empty() ? nullptr : AiNodeLookUpByName(universe, rimLight.c_str());
     if (rimLightNode) {
         std::string rimLightUsdName = GetArnoldNodeName(rimLightNode);
-        // At this point the attribute should already exist, as it 
+        // At this point the attribute should already exist, as it
         // was created by _WriteArnoldParameters if not empty
         UsdAttribute rimLightsAttr = prim.GetAttribute(TfToken("inputs:rim_light"));
         if (rimLightsAttr)
             rimLightsAttr.Set(VtValue(rimLightUsdName.c_str()));
     }
 
-    // Same as above, the attribute "lights" is a string pointing at node names, 
+    // Same as above, the attribute "lights" is a string pointing at node names,
     // that need to be adapted. This attribute is more complicated than "rim_light"
     // because the string can concatenate multiple light names, separated by semicolor
     // or empty space. So we need to split each light name, convert them separately,
@@ -109,7 +108,7 @@ void UsdArnoldWriteToon::Write(const AtNode *node, UsdArnoldWriter &writer)
             AtNode *lightNode = AiNodeLookUpByName(universe, lightName.c_str());
             if (lightNode == nullptr)
                 continue;
-            
+
             if (!lights.empty())
                 lights += ";";
             lights += GetArnoldNodeName(lightNode);
