e529897472f3afc87d4240545080fa63a7e60755
Pal Mezei <sirpalee@users.noreply.github.com>
2020-08-10 18:46:06 +1000

    Fixing building for Katana on Windows. (#499)
    Fixes #498

--- tools/utils/dependencies.py
+++ tools/utils/dependencies.py
@@ -14,80 +14,76 @@
 # limitations under the License.
 import build_tools, system
 
+def get_boost_lib(env, lib):
+    return env['BOOST_LIB_NAME'] % lib
+
 def add_optional_libs(env, libs):
     if env['USD_HAS_PYTHON_SUPPORT']:
-        return libs + [env['PYTHON_LIB_NAME'], env['BOOST_LIB_NAME'] % 'python']
+        return libs + [env['PYTHON_LIB_NAME'], get_boost_lib(env, 'python')]
     else:
         return libs
+
+def get_tbb_lib(env):
+    return env['TBB_LIB_NAME'] % 'tbb'
+
+def add_plugin_deps(env, sources, libs, needs_dl):
+    if env['USD_BUILD_MODE'] == 'monolithic':
+        usd_deps = [
+            env['USD_MONOLITHIC_LIBRARY'],
+            get_tbb_lib(env),
+        ]
+        if needs_dl and system.IS_LINUX:
+            usd_deps = libs + ['dl']
+        return (sources, add_optional_libs(env, usd_deps))
+    else:
+        usd_deps = [get_tbb_lib(env)]
+        usd_libs, usd_sources = build_tools.link_usd_libraries(env, libs)
+        usd_deps = usd_deps + usd_libs
+        source_files = sources + usd_sources
+        if needs_dl and system.IS_LINUX:
+            usd_deps = usd_deps + ['dl']
+        return (source_files, add_optional_libs(env, usd_deps))
 
 # Returns a list of usd dependencies and source files.
 # This only works with monolithic and shared usd dependencies.
 def render_delegate(env, sources):
-    if (env['USD_BUILD_MODE'] == 'monolithic'):
-        usd_deps = [
-            env['USD_MONOLITHIC_LIBRARY'],
-            'tbb',
-        ]
-        if (system.IS_LINUX):
-            usd_deps = usd_deps + ['dl']
-        return (sources, add_optional_libs(env, usd_deps))
-    else:
-        usd_libs = [
-            'arch',
-            'plug',
-            'tf',
-            'vt',
-            'gf',
-            'work',
-            'hf',
-            'hd',
-            'hdx',
-            'sdf',
-            'usdImaging',
-            'usdLux',
-            'pxOsd',
-        ]
-
-        usd_deps = ['tbb']
-        usd_libs, usd_sources = build_tools.link_usd_libraries(env, usd_libs)
-        usd_deps = usd_deps + usd_libs
-        source_files = sources + usd_sources
-        if (system.IS_LINUX):
-            usd_deps = usd_deps + ['dl']
-        return (source_files, add_optional_libs(env, usd_deps))
+    usd_libs = [
+        'arch',
+        'plug',
+        'tf',
+        'vt',
+        'gf',
+        'work',
+        'hf',
+        'hd',
+        'hdx',
+        'sdf',
+        'usdImaging',
+        'usdLux',
+        'pxOsd',
+    ]
+    return add_plugin_deps(env, sources, usd_libs, True)
 
 # This only works with monolithic and shared usd dependencies.
 def ndr_plugin(env, sources):
-    if env['USD_BUILD_MODE'] == 'monolithic':
-        usd_deps = [
-            env['USD_MONOLITHIC_LIBRARY'],
-            'tbb',
-        ]
-        return (sources, add_optional_libs(env, usd_deps))
-    else:
-        usd_libs = [
-            'arch',
-            'tf',
-            'gf',
-            'vt',
-            'ndr',
-            'sdr',
-            'sdf',
-            'usd',
-        ]
-
-        usd_deps = ['tbb']
-        usd_libs, usd_sources = build_tools.link_usd_libraries(env, usd_libs)
-        usd_deps = usd_deps + usd_libs
-        source_files = sources + usd_sources
-        return (source_files, add_optional_libs(env, usd_deps))
+    usd_libs = [
+        'arch',
+        'tf',
+        'gf',
+        'vt',
+        'ndr',
+        'sdr',
+        'sdf',
+        'usd',
+    ]
+    return add_plugin_deps(env, sources, usd_libs, False)
 
 def translator(env, sources):
     if env['USD_BUILD_MODE'] == 'monolithic':
         usd_deps = [
             'usd_translator',
             env['USD_MONOLITHIC_LIBRARY'],
-            'tbb',
+            get_tbb_lib(env),
         ]
         return (sources, add_optional_libs(env, usd_deps))
     elif env['USD_BUILD_MODE'] == 'static':
@@ -95,7 +91,7 @@ def translator(env, sources):
         if system.IS_WINDOWS:
             usd_deps = [
                 '-WHOLEARCHIVE:libusd_m', 
-                'tbb',
+                get_tbb_lib(env),
                 'Ws2_32',
                 'Dbghelp',
                 'Shlwapi', 
@@ -104,7 +100,7 @@ def translator(env, sources):
         else:
             usd_deps = [
                 'libusd_m', 
-                'tbb',
+                get_tbb_lib(env),
             ]
 
             if system.IS_LINUX:
@@ -126,7 +122,7 @@ def translator(env, sources):
             'usdRender',
         ]
 
-        usd_deps = ['tbb']
+        usd_deps = [get_tbb_lib(env)]
 
         usd_libs, usd_sources = build_tools.link_usd_libraries(env, usd_libs)
         source_files = sources + usd_sources
