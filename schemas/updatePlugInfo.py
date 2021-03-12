317b127cc77494930f7e9156455dd4965ac8f1b5
Pal Mezei <sirpalee@users.noreply.github.com>
2021-03-12 20:20:25 +1100

    Removing the c++ plugin/python lib build from schemas and installing the rest under a different directory. (#706)
    Fixes #705

--- schemas/updatePlugInfo.py
+++ schemas/updatePlugInfo.py
@@ -22,14 +22,14 @@ def update_plug_info(plug_info):
     contents = contents.replace('@PLUG_INFO_ROOT@', '..')
     contents = contents.replace('@PLUG_INFO_RESOURCE_PATH@', 'resources')
     if platform.system().lower() == 'linux':
-        contents = contents.replace('"LibraryPath": "../../libusd.so"', '"LibraryPath": "../../libusdArnold.so"')
-        contents = contents.replace('@PLUG_INFO_LIBRARY_PATH@', '../../libusdArnold.so')
+        contents = contents.replace('"LibraryPath": "../../libusd.so"', '"LibraryPath": ""')
+        contents = contents.replace('@PLUG_INFO_LIBRARY_PATH@', '')
     elif platform.system().lower() == 'darwin':
-        contents = contents.replace('"LibraryPath": "../../libusd.dylib"', '"LibraryPath": "../../libusdArnold.dylib"')
-        contents = contents.replace('@PLUG_INFO_LIBRARY_PATH@', '../../libusdArnold.dylib')
+        contents = contents.replace('"LibraryPath": "../../libusd.dylib"', '"LibraryPath": ""')
+        contents = contents.replace('@PLUG_INFO_LIBRARY_PATH@', '')
     else:
-        contents = contents.replace('"LibraryPath": "../../usd.dll"', '"LibraryPath": "../../usdArnold.dll"')
-        contents = contents.replace('@PLUG_INFO_LIBRARY_PATH@', '../../usdArnold.dll')
+        contents = contents.replace('"LibraryPath": "../../usd.dll"', '"LibraryPath": ""')
+        contents = contents.replace('@PLUG_INFO_LIBRARY_PATH@', '')
     f = open(plug_info, 'w')
     f.write(contents)
 
