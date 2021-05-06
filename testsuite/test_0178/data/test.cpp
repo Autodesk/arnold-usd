be88b0025c86b0190429c4802c87bbf2980c6c0f
Pal Mezei <pal.mezei@autodesk.com>
2021-05-06 15:42:10 +1000

    Updating test_0178 with more examples. #777

--- testsuite/test_0178/data/test.cpp
+++ testsuite/test_0178/data/test.cpp
@@ -6,55 +6,72 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <vector>
+
+/// Testing animated parameter export using skydome_light.exposure.
+/// @param samples List of samples to test.
+/// @param filename USD filename to use.
+bool testParameter(const std::vector<float>& samples, const std::string& filename)
+{
+    const AtString nodeName {"/aiSkyDomeLight1"};
+    const AtString paramName {"exposure"};
+    float frame = 1.0f;
+    
+    for (const auto& sample: samples) {
+        auto* universe = AiUniverse();
+        auto* light = AiNode(universe, AtString{"skydome_light"});
+        AiNodeSetFlt(light, paramName, sample);
+        AiNodeSetStr(light, AtString{"name"}, nodeName);
+        auto* params = AiParamValueMap();
+        AiParamValueMapSetFlt(params, AtString{"frame"}, frame);
+        if (frame > 1.0f) {
+            AiParamValueMapSetBool(params, AtString{"append"}, true);
+        }
+        AiSceneWrite(universe, filename.c_str(), params);
+        AiParamValueMapDestroy(params);
+        AiUniverseDestroy(universe);
+        frame += 1.0f;
+    }
+
+    bool success = true;
+    frame = 1.0f;
+    for (const auto& sample: samples) {
+        auto* universe = AiUniverse();
+        auto* params = AiParamValueMap();
+        AiParamValueMapSetFlt(params, AtString("frame"), frame);
+        AiSceneLoad(universe, filename.c_str(), params);
+        auto* light = AiNodeLookUpByName(universe, nodeName);
+        if (light == nullptr) {
+            AiMsgError("Could not find %s", nodeName.c_str());
+            AiParamValueMapDestroy(params);
+            AiUniverseDestroy(universe);
+            success = false;
+            break;
+        }
+
+        if (AiNodeGetFlt(light, paramName) != sample) {
+            AiMsgError("%s value isn't correct for frame %d : found %d instead of %d",
+                       paramName.c_str(), frame, AiNodeGetFlt(light, paramName), sample);
+            success = false;
+        }
+        AiParamValueMapDestroy(params);
+        AiUniverseDestroy(universe);
+        frame += 1.0f;
+    }
+
+    return success;
+}
 
 int main(int argc, char **argv)
 {
     AiMsgSetConsoleFlags(AI_LOG_ALL);
     AiBegin();
-    AtUniverse *universe1 = AiUniverse();
-    AiSceneLoad(universe1, "scene.01.ass", nullptr);
-    AtParamValueMap* params = AiParamValueMap();
-    AiParamValueMapSetFlt(params, AtString("frame"), 1.f);    
-    AiSceneWrite(universe1, "scene_exported.usda", params);
-    AiUniverseDestroy(universe1);
 
-    AtUniverse *universe2 = AiUniverse();
-    AiSceneLoad(universe2, "scene.02.ass", nullptr);
-    AiParamValueMapSetFlt(params, AtString("frame"), 2.f);    
-    AiParamValueMapSetBool(params, AtString("append"), true);    
-    AiSceneWrite(universe2, "scene_exported.usda", params);
-    AiUniverseDestroy(universe2);
-
-    AtUniverse *universe3 = AiUniverse();
-    AiSceneLoad(universe3, "scene.03.ass", nullptr);
-    AiParamValueMapSetFlt(params, AtString("frame"), 3.f);    
-    AiParamValueMapSetBool(params, AtString("append"), true);
-    AiSceneWrite(universe3, "scene_exported.usda", params);
-    AiUniverseDestroy(universe3);
-
-    AiParamValueMapDestroy(params);
-    bool success = true;
-    for (int i = 1; i <= 3; ++i) {
-        AtUniverse *universe = AiUniverse();
-        params = AiParamValueMap();
-        AiParamValueMapSetFlt(params, AtString("frame"), (float) i);
-        AiSceneLoad(universe, "scene_exported.usda", params);
-        AtNode *light = AiNodeLookUpByName(universe, "/aiSkyDomeLight1");
-        if (light == nullptr) {
-            AiMsgError("Could not find /aiSkyDomeLight1");
-            success = false;
-            break;
-        }
-
-        if (AiNodeGetFlt(light, "exposure") != (float)i) {
-            success = false;
-            AiMsgError("Exposure value isn't correct for frame %d : found %d instead of %d", i, AiNodeGetFlt(light, "exposure"), i);
-            break;
-        }
-        AiParamValueMapDestroy(params);
-        AiUniverseDestroy(universe);
-    }   
-        
+    bool sucess =
+        testParameter({1.0f, 2.0f, 3.0f, 4.0f}, "increasing_samples.usda") &&
+        testParameter({1.0f, 2.0f, 2.0f, 2.0f, 2.0f, 1.0f, 2.0f}, "wavy_samples.usda") &&
+        testParameter({1.0f, 1.0f, 1.0f, 2.0f}, "flat_start.usda");
     AiEnd();
-    return success ? 0 : 1;
+
+    return sucess ? 0 : 1;
 }
