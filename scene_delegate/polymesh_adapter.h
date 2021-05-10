87ec7262b1835a2bedd1c69a49d5817cf2438797
Pal Mezei <pal.mezei@autodesk.com>
2021-05-10 14:05:20 +1000

    Documenting the scene_delegate module. (#764)

--- scene_delegate/polymesh_adapter.h
+++ scene_delegate/polymesh_adapter.h
@@ -23,22 +23,48 @@
 
 PXR_NAMESPACE_OPEN_SCOPE
 
+/// @class ImagingArnoldPerspCameraAdapter
+///
+/// Adapter for polymesh.
 class ImagingArnoldPolymeshAdapter : public ImagingArnoldRprimAdapter {
 public:
     using BaseAdapter = ImagingArnoldRprimAdapter;
 
+    /// Tells if the polymesh adapter can work with a given Arnold scene delegate.
+    ///
+    /// @param proxy Pointer to the ImagingArnoldDelegateProxy.
+    /// @return True if the adapter works with the Arnold scene delegate, false otherwise.
     IMAGINGARNOLD_API
     bool IsSupported(ImagingArnoldDelegateProxy* proxy) const override;
 
+    /// Populates a given Arnold scene delegate with the Hydra primitive required by the polymesh adapter.
+    ///
+    /// @param node Pointer to the Arnold node.
+    /// @param proxy Pointer to the ImagingArnoldDelegateProxy.
+    /// @param id Path of the Hydra primitive.
     IMAGINGARNOLD_API
     void Populate(AtNode* node, ImagingArnoldDelegateProxy* proxy, const SdfPath& id) override;
 
+    /// Gets the mesh topology of an Arnold polymesh.
+    ///
+    /// @param node Pointer to the Arnold polymesh node.
+    /// @return Topology of a Hydra mesh.
     IMAGINGARNOLD_API
     HdMeshTopology GetMeshTopology(const AtNode* node) const override;
 
+    /// Gets the primvar descriptors of an Arnold polymesh.
+    ///
+    /// @param node Pointer to the Arnold polymesh.
+    /// @param interpolation Interpolation of the primvar descriptors to query.
+    /// @return Primvar descriptors of a given interpolation type, empty vector if none available.
     IMAGINGARNOLD_API
     HdPrimvarDescriptorVector GetPrimvarDescriptors(const AtNode* node, HdInterpolation interpolation) const override;
 
+    /// Gets a named value from an Arnold polymesh.
+    ///
+    /// @param node Pointer to the Arnold polymesh.
+    /// @param key Name of the value.
+    /// @return Value of a given name named value, empty VtValue if not available.
     IMAGINGARNOLD_API
     VtValue Get(const AtNode* node, const TfToken& key) const override;
 };
