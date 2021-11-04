5d7fa0d41356516c62b85e0a4ed19f5f07f57cda
Pal Mezei <sirpalee@users.noreply.github.com>
2021-11-04 22:15:29 +1100

    Setting the returned version to default in the Sdr plugin. (#907)
    Fixes #906

--- ndr/discovery.cpp
+++ ndr/discovery.cpp
@@ -62,14 +62,14 @@ NdrNodeDiscoveryResultVec NdrArnoldDiscoveryPlugin::DiscoverNodes(const Context&
         TfToken filename("<built-in>");
         prim.GetMetadata(_tokens->filename, &filename);
         ret.emplace_back(
-            NdrIdentifier(TfStringPrintf("arnold:%s", shaderName.GetText())), // identifier
-            NdrVersion(AI_VERSION_ARCH_NUM, AI_VERSION_MAJOR_NUM),            // version
-            shaderName,                                                       // name
-            _tokens->shader,                                                  // family
-            _tokens->arnold,                                                  // discoveryType
-            _tokens->arnold,                                                  // sourceType
-            filename,                                                         // uri
-            filename                                                          // resolvedUri
+            NdrIdentifier(TfStringPrintf("arnold:%s", shaderName.GetText())),     // identifier
+            NdrVersion(AI_VERSION_ARCH_NUM, AI_VERSION_MAJOR_NUM).GetAsDefault(), // version
+            shaderName,                                                           // name
+            _tokens->shader,                                                      // family
+            _tokens->arnold,                                                      // discoveryType
+            _tokens->arnold,                                                      // sourceType
+            filename,                                                             // uri
+            filename                                                              // resolvedUri
         );
     }
     return ret;
