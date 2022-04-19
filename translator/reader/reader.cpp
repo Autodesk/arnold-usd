f54fe8e144409f3a3c3661c313fa06b91afd861d
Sebastien Blaineau-Ortega <sebastien.blaineau.ortega@autodesk.com>
2022-04-19 14:05:51 +0200

    Support instanceable prims with variant & transform overrides (#1127)
    * Support instanceable prims with variant overrides #1122
    
    * Adding test 222 #11222
    
    * Only write variant overrides for external files when needed #1122
    
    * Support transformation overrides on instanceable prims #1122

--- translator/reader/reader.cpp
+++ translator/reader/reader.cpp
@@ -19,6 +19,7 @@
 #include <pxr/usd/pcp/layerStack.h>
 #include <pxr/usd/pcp/node.h>
 #include <pxr/usd/usd/prim.h>
+#include <pxr/usd/usd/variantSets.h>
 #include <pxr/usd/usd/primRange.h>
 #include <pxr/usd/usd/stage.h>
 #include <pxr/usd/usd/primCompositionQuery.h>
@@ -548,6 +549,7 @@ void UsdArnoldReader::ReadStage(UsdStageRefPtr stage, const std::string &path)
 void UsdArnoldReader::ReadPrimitive(const UsdPrim &prim, UsdArnoldReaderContext &context, bool isInstance)
 {
     std::string objName = prim.GetPath().GetText();
+    const TimeSettings &time = context.GetTimeSettings();
 
     if (isInstance) {
 #if PXR_VERSION >= 2011
@@ -555,44 +557,116 @@ void UsdArnoldReader::ReadPrimitive(const UsdPrim &prim, UsdArnoldReaderContext 
 #else
         auto proto = prim.GetMaster();
 #endif
-        // If this instance is pointing to a reference file, we want to treat it in a special way 
-        // USD creates a prim e.g. /__Prototype1 that represents this referenced file. But if there 
-        // are multiple references in the scene, then their name is not always consistent. Therefore
-        // we need to ensure we're not giving such an object path in nested USD procedurals, otherwise
-        // we get random switches between the referenced files (see #1021). To prevent that we store
-        // every instance referenced files, along with their corresponding primitive name. This will be 
-        // used later in ProcessConnection, to set the proper filename in the nested procedural
-        if (prim.HasAuthoredReferences()) {
-            UsdPrimCompositionQuery compQuery = UsdPrimCompositionQuery::GetDirectReferences(prim);
-            std::vector<UsdPrimCompositionQueryArc> compArcs = compQuery.GetCompositionArcs();
-            if (compArcs.size() > 0) {
-                PcpNodeRef nodeRef = compArcs[0].GetTargetNode();
-                PcpLayerStackRefPtr stackRef = nodeRef.GetLayerStack();
-                auto layers = stackRef->GetLayers();
-                if (layers.size() > 0) {
-                    LockReader();
-                    // store the reference filename in a map, where the key is the prototype prim name
-                    
-                    auto &ref = _referencesMap[proto.GetPath().GetText()];
-                    // the map value is a pair of strings. The first element is the filename
-                    // and the second is the object path
-                    ref.first = layers[0]->GetRealPath();
-                    // default to the current filename if no layer path is defined (#1093)
-                    if (ref.first.empty())
-                        ref.first = GetFilename();
-                    ref.second = nodeRef.GetPath().GetText();
-                    UnlockReader();
+        if (proto) {
+            AtArray *protoMatrix = nullptr;
+            // If this instance is pointing to a reference file, we want to treat it in a special way 
+            // USD creates a prim e.g. /__Prototype1 that represents this referenced file. But if there 
+            // are multiple references in the scene, then their name is not always consistent. Therefore
+            // we need to ensure we're not giving such an object path in nested USD procedurals, otherwise
+            // we get random switches between the referenced files (see #1021). To prevent that we store
+            // every instance referenced files, along with their corresponding primitive name. This will be 
+            // used later in ProcessConnection, to set the proper filename in the nested procedural
+            if (prim.HasAuthoredReferences()) {
+                UsdPrimCompositionQuery compQuery = UsdPrimCompositionQuery::GetDirectReferences(prim);
+                std::vector<UsdPrimCompositionQueryArc> compArcs = compQuery.GetCompositionArcs();
+                if (compArcs.size() > 0) {
+                    PcpNodeRef nodeRef = compArcs[0].GetTargetNode();
+                    PcpLayerStackRefPtr stackRef = nodeRef.GetLayerStack();
+                    auto layers = stackRef->GetLayers();
+                    if (layers.size() > 0) {
+                        LockReader();
+                        // store the reference filename in a map, where the key is the prototype prim name
+                        
+                        auto &ref = _referencesMap[proto.GetPath().GetText()];
+                        // the map value is a pair of strings. The first element is the filename
+                        // and the second is the object path
+                        ref.filename = layers[0]->GetRealPath();
+                        // default to the current filename if no layer path is defined (#1093)
+                        if (ref.filename.empty())
+                            ref.filename = GetFilename();
+                        // objectPath is the name of the prototype primitive. Note that this is different from
+                        // the actual "proto" path name, which in practice will be e.g. __Prototype1, and that
+                        // will not be consistent.
+                        ref.objectPath = nodeRef.GetPath().GetText();
+
+                        // Get the variants map for the prototype prim
+                        bool hasVariants = prim.HasVariantSets();
+                        std::unordered_map<std::string, std::string> protoVariants;
+
+                        auto GetPrototypeData = [](const UsdPrim &prim, bool hasVariants, 
+                            const TimeSettings &time,
+                            std::unordered_map<std::string, std::string> &variants) 
+                        {
+                            if (!prim)
+                                return (AtArray*)nullptr;
+                            if (hasVariants) {
+                                // add the list of prototype's variant sets / variants to our map
+                                UsdVariantSets varSets = prim.GetVariantSets();
+                                std::vector<std::string> varSetNames = varSets.GetNames();
+                                for (auto &varSet : varSetNames) {
+                                    variants[varSet] = varSets.GetVariantSelection(varSet);
+                                }
+                            }
+                            // get the prototype's local matrix, as we need to "remove" it
+                            // from the instancable prim transform
+                            if (prim.IsA<UsdGeomXformable>()) {
+                                return ReadLocalMatrix(prim, time);
+                            }
+                            return (AtArray*)nullptr;
+                        };
+
+                        // We need to get informations about the actual primitive that is being instanced
+                        // (variants, transform). But the "proto" primitive that we have is just an intermediate primitive
+                        // so we need to load it from the stage, either the current one, or a new that loads a given
+                        // SdfLayer (when the reference is pointing to another file)
+                        UsdPrim protoPrim = _stage->GetPrimAtPath(nodeRef.GetPath());
+                        if (protoPrim) {
+                            protoMatrix = GetPrototypeData(protoPrim, hasVariants, time, protoVariants);
+                        } else {
+                            UsdStageRefPtr layerStage = UsdStage::Open(layers[0], UsdStage::LoadNone);
+                            protoPrim = layerStage->GetPrimAtPath(nodeRef.GetPath());
+                            protoMatrix = GetPrototypeData(protoPrim, hasVariants, time, protoVariants);
+                        }
+                        // Get all the variants for this instanceable primitive
+                        // and add them to a map. Note that we can associate this with the proto path, because
+                        // USD will create different prototypes when the same proto is instanciated with different
+                        // variants overrides (#1122)
+                        UsdVariantSets varSets = prim.GetVariantSets();
+                        std::vector<std::string> varSetNames = varSets.GetNames();
+                        for (auto &varSet : varSetNames) {
+                            std::string variantValue = varSets.GetVariantSelection(varSet);
+                            auto &it = protoVariants.find(varSet);
+                            // If the instanceable prim variant is the same as the prototype's one,
+                            // then there's no need to store the current variant
+                            if (it != protoVariants.end() && it->second == variantValue)
+                                continue;
+        
+                            ref.variants[varSet] = variantValue;
+                        }
+                        UnlockReader();
+                    }                
                 }
             }
-        }
-        
-        if (proto) { 
-            
-            const TimeSettings &time = context.GetTimeSettings();
-            
+
             AtNode *ginstance = context.CreateArnoldNode("ginstance", objName.c_str());
-            if (prim.IsA<UsdGeomXformable>())
+            if (prim.IsA<UsdGeomXformable>()) {
                 ReadMatrix(prim, ginstance, time, context);
+                if (protoMatrix) {
+                    // for each key, divide the ginstance matrix by the protoMatrix
+                    AtArray *gMtx = AiNodeGetArray(ginstance, str::matrix);
+                    size_t numKeys = AiArrayGetNumKeys(gMtx);
+                    size_t numProtoKeys = AiArrayGetNumKeys(protoMatrix);
+                    for (size_t i = 0; i < numKeys; ++i) {
+                        AtMatrix mtx = AiArrayGetMtx(gMtx, i);
+                        AtMatrix protoInvMtx = AiM4Invert(AiArrayGetMtx(protoMatrix, AiMax(i, numProtoKeys - 1)));
+                        mtx = AiM4Mult(protoInvMtx, mtx);
+                        AiArraySetMtx(gMtx, i, mtx);
+                    }
+                }
+            }
+            if (protoMatrix)
+                AiArrayDestroy(protoMatrix);
+
             AiNodeSetFlt(ginstance, str::motion_start, time.motionStart);
             AiNodeSetFlt(ginstance, str::motion_end, time.motionEnd);
             AiNodeSetByte(ginstance, str::visibility, AI_RAY_ALL);
@@ -605,7 +679,6 @@ void UsdArnoldReader::ReadPrimitive(const UsdPrim &prim, UsdArnoldReaderContext 
                 UsdArnoldPrimReader::ReadPrimvars(prim, ginstance, time, jobContext);
                 UsdArnoldPrimReader::ReadArnoldParameters(prim, jobContext, ginstance, time, "primvars:arnold");
             }
-
             
             // Add a connection from this instance to the prototype. It's likely not going to be
             // Arnold, and will therefore appear as a "dangling" connection. The prototype will
@@ -1101,6 +1174,7 @@ bool UsdArnoldReaderThreadContext::ProcessConnection(const Connection &connectio
 
                         std::string nestedFilename = _reader->GetFilename().c_str();
                         std::string nestedObjectPath = connection.target;
+                        std::string nestedOverride;
                         int cacheId = _reader->GetCacheId();
 
                         // If this instance is pointing to a reference file, 
@@ -1109,9 +1183,8 @@ bool UsdArnoldReaderThreadContext::ProcessConnection(const Connection &connectio
                         // To prevent random results (see #1021), we set in this case the referenced filename
                         // on the child usd procedural, instead of the "current" USD filename
                         if (cacheId == 0)
-                            _reader->GetReferencePath(prim.GetPath().GetText(), nestedFilename, nestedObjectPath);
-                            
-                        
+                            _reader->GetReferencePath(prim.GetPath().GetText(), nestedFilename, nestedObjectPath, nestedOverride);
+                                                    
                         AiNodeSetStr(target, str::filename, AtString(nestedFilename.c_str()));
                         AiNodeSetStr(target, str::object_path, AtString(nestedObjectPath.c_str()));
                         AiNodeSetInt(target, str::cache_id, cacheId);
@@ -1119,9 +1192,20 @@ bool UsdArnoldReaderThreadContext::ProcessConnection(const Connection &connectio
                         AiNodeSetFlt(target, str::frame, time.frame); // give it the desired frame
                         AiNodeSetFlt(target, str::motion_start, time.motionStart);
                         AiNodeSetFlt(target, str::motion_end, time.motionEnd);
-                        const AtArray *overrides = _reader->GetOverrides();
+                        const AtArray *readerOverrides = _reader->GetOverrides();
+                        AtArray *overrides = (readerOverrides != nullptr) ? AiArrayCopy(readerOverrides) : nullptr;
+
+                        if (!nestedOverride.empty()) {
+                            if (overrides) {
+                                AiArrayResize(overrides, AiArrayGetNumElements(overrides) + 1, 1);
+                            }
+                            else  {
+                                overrides = AiArrayAllocate(1, 1, AI_TYPE_STRING);
+                            }
+                            AiArraySetStr(overrides, AiArrayGetNumElements(overrides) - 1, nestedOverride.c_str());
+                        }
                         if (overrides)
-                            AiNodeSetArray(target, str::overrides, AiArrayCopy(overrides));
+                            AiNodeSetArray(target, str::overrides, overrides);
                         // Hide the prototype, we'll only want the instance to be visible
                         AiNodeSetByte(target, str::visibility, 0);
                         AiNodeSetInt(target, str::threads, _reader->GetThreadCount());
@@ -1242,17 +1326,47 @@ bool UsdArnoldReaderContext::GetPrimVisibility(const UsdPrim &prim, float frame)
     return true;
 }
 
-bool UsdArnoldReader::GetReferencePath(const std::string &primName, std::string &filename, std::string &objectPath)
+bool UsdArnoldReader::GetReferencePath(const std::string &primName, std::string &filename, 
+                                        std::string &objectPath, std::string &override)
 {
     bool success = false;
+    std::unordered_map<std::string, std::string> variantsMap;
     LockReader();
     auto referencePath = _referencesMap.find(primName);
     if (referencePath != _referencesMap.end()) {
-        filename = referencePath->second.first;
-        objectPath = referencePath->second.second;
+        filename = referencePath->second.filename;
+        objectPath = referencePath->second.objectPath;
+        // copy the variants map while we're locked. We'll
+        // serialize it after we unlock
+        variantsMap = referencePath->second.variants;
         success = true;
     }
     UnlockReader();
+
+    if (!variantsMap.empty() && !objectPath.empty()) {
+        // Some variants were assigned to this prototype, 
+        // we want to check first if they're already part of the 
+        // actual prototype. If that's not the case, it means that 
+        // we need to override the variant here. The way we can do this
+        // is by adding an "overrides" statement in the nested usd procedural,
+        // that will modify the variant for this prim.
+        override = "#usda 1.0\nover \"";
+        if (objectPath[0] == '/')
+            override += objectPath.substr(1);
+        else 
+            override += objectPath;
+        
+        override += "\" (variants = {\n";
+        for (auto &variant : variantsMap) {
+            // At this point we know the variant is different from the prototype
+            override += "string ";
+            override += variant.first;
+            override += " = \"";
+            override += variant.second;
+            override += "\"\n";
+        }
+        override += "}){}";
+    }
     return success;
 }
 
