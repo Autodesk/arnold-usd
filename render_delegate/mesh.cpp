Autodesk/arnold-usd

commit 6c371f71a8453485ffb2d24c9736edd08520928a

Support Arnold light attributes in meshes #1146 (#1147)

render_delegate/mesh.cpp
@@ -349,6 +349,8 @@ void HdArnoldMesh::Sync(
         _autobumpVisibilityFlags.ClearPrimvarFlags();
         param.Interrupt();
         const auto isVolume = _IsVolume();
+        AtNode *meshLight = _GetMeshLight(sceneDelegate, id);
+
         for (auto& primvar : _primvars) {
             auto& desc = primvar.second;
             if (!desc.NeedsUpdate()) {
@@ -356,6 +358,28 @@ void HdArnoldMesh::Sync(
             }
 
             if (desc.interpolation == HdInterpolationConstant) {
+                // If we have a mesh light, we want to check for light attributes 
+                // with a "light:" namespace
+                if (meshLight) {
+                    // ignore the attribute arnold:light:enable which is just meant
+                    // to trigger the creation of the mesh light
+                    if (primvar.first == _tokens->arnold_light)
+                        continue;
+                    std::string primvarStr = primvar.first.GetText();
+                    const static std::string s_lightPrefix = "arnold:light:";
+                    // check if the attribute starts with "arnold:light:"
+                    if (primvarStr.length() > s_lightPrefix.length() && 
+                        primvarStr.substr(0, s_lightPrefix.length()) == s_lightPrefix) {
+                        // we want to read this attribute and set it in the light node. We need to 
+                        // modify the attribute name so that we remove the light prefix
+                        primvarStr.erase(7, 6);
+                        HdArnoldSetConstantPrimvar(
+                            _geometryLight, TfToken(primvarStr.c_str()), desc.role, desc.value, 
+                            nullptr, nullptr, nullptr);
+                        continue;
+                    }
+                }
+
                 HdArnoldSetConstantPrimvar(
                     GetArnoldNode(), primvar.first, desc.role, desc.value, &_visibilityFlags, &_sidednessFlags,
                     &_autobumpVisibilityFlags);
@@ -460,5 +484,32 @@ HdDirtyBits HdArnoldMesh::GetInitialDirtyBitsMask() const
 }
 
 bool HdArnoldMesh::_IsVolume() const { return AiNodeGetFlt(GetArnoldNode(), str::step_size) > 0.0f; }
+
+AtNode *HdArnoldMesh::_GetMeshLight(HdSceneDelegate* sceneDelegate, const SdfPath& id)
+{
+    bool hasMeshLight = false;
+    VtValue lightValue = sceneDelegate->Get(id, _tokens->arnold_light);
+    if (lightValue.IsHolding<bool>()) {
+        hasMeshLight = lightValue.UncheckedGet<bool>();            
+    }
+    
+    if (hasMeshLight) {
+        if (_geometryLight == nullptr) {
+            // We need to create the mesh light, pointing to the current mesh.
+            // We'll name it based on the mesh name, adding a light suffix
+            std::string lightName = AiNodeGetName(GetArnoldNode());
+            lightName += "/light";
+            _geometryLight = AiNode(AiNodeGetUniverse(GetArnoldNode()), 
+                        str::mesh_light, AtString(lightName.c_str()));
+        }
+        AiNodeSetPtr(_geometryLight, str::mesh, (void*)GetArnoldNode());
+    } else if (_geometryLight) {
+        // if a geometry light was previously set and it's not there anymore, 
+        // we need to clear it now
+        AiNodeDestroy(_geometryLight);
+        _geometryLight = nullptr;    
+    }
+    return _geometryLight;
+}
 
 PXR_NAMESPACE_CLOSE_SCOPE

common/constant_strings.h
@@ -54,6 +54,7 @@ ASTR2(_min, "min");
 ASTR2(arnold__attributes, "arnold::attributes");
 ASTR2(arnold_camera, "arnold:camera");
 ASTR2(arnold_filename, "arnold:filename");
+ASTR2(arnold_light, "arnold:light");
 ASTR2(arnold_node_entry, "arnold:node_entry");
 ASTR2(arnold_prefix, "arnold:");
 ASTR2(autobump_visibility_prefix, "autobump_visibility:");
@@ -66,6 +67,7 @@ ASTR2(deformKeys, "arnold:deform_keys");
 ASTR2(houdiniFps, "houdini:fps");
 ASTR2(hydraPrimId, "hydra_primId");
 ASTR2(inputs_code, "inputs:code");
+ASTR2(primvars_arnold_light, "primvars:arnold:light");
 ASTR2(log_file, "log:file");
 ASTR2(log_verbosity, "log:verbosity");
 ASTR2(primvars_arnold_subdiv_type, "primvars:arnold:subdiv_type");

render_delegate/mesh.h
@@ -94,11 +94,15 @@ protected:
     HDARNOLD_API
     bool _IsVolume() const;
 
+    HDARNOLD_API
+    AtNode *_GetMeshLight(HdSceneDelegate* sceneDelegate, const SdfPath& id);
+
     HdArnoldPrimvarMap _primvars;     ///< Precomputed list of primvars.
     HdArnoldSubsets _subsets;         ///< Material ids from subsets.
     VtIntArray _vertexCounts;         ///< Vertex Counts array for reversing vertex and primvar polygon order.
     size_t _vertexCountSum = 0;       ///< Sum of the vertex counts array.
     size_t _numberOfPositionKeys = 1; ///< Number of vertex position keys for the mesh.
+    AtNode *_geometryLight = nullptr; ///< Eventual mesh light for this polymesh
 };
 
 PXR_NAMESPACE_CLOSE_SCOPE

testsuite/test_0226/README
@@ -1,0 +1,7 @@
+Arnold Light attributes on meshes
+
+See #1146
+
+author: sebastien.ortega
+
+PARAMS: {'scene':'test.usda'}

testsuite/test_0226/ref/reference.log
@@ -1,0 +1,235 @@
+00:00:00    91MB         | log started Fri May  6 00:05:45 2022
+00:00:00    91MB         | Arnold 7.1.1.0 [eba1345a] windows clang-10.0.1 oiio-2.4.0 osl-1.12.0 vdb-7.1.1 adlsdk-6.3.1.44 clmhub-2.0.0.235 rlm-14.1.3 optix-6.6.0 2022/03/28 17:43:48
+00:00:00    91MB         | running on REMS4QL1726, pid=88052
+00:00:00    91MB         |  2 x Intel(R) Xeon(R) Gold 5218 CPU @ 2.30GHz (32 cores, 64 logical) with 65154MB
+00:00:00    94MB         |  NVIDIA driver version 472.39 (Optix 60801)
+00:00:00    94MB         |  GPU 0: Quadro RTX 4000 @ 1545MHz (compute 7.5) with 8192MB (5376MB available) (NVLink:0)
+00:00:00    94MB         |  Windows 10 (version 10.0, build 19044)
+00:00:00    94MB         |  soft limit for open files is set at 2048
+00:00:00    94MB         |  
+00:00:00   112MB         | [color_manager_ocio] default ocio.config found in C:\arnold\sdk\latest\bin\../ocio/configs/arnold/config.ocio
+00:00:00   112MB         | loading plugins from C:\arnold\arnold-usd\build\windows_x86_64\msvc_opt\usd-0.21.8_arnold-7.1.1.0\procedural ...
+00:00:00   116MB         |  usd_proc.dll: usd uses Arnold 7.1.1.0
+00:00:00   116MB         | loaded 1 plugins from 1 lib(s) in 0:00.08
+00:00:00   116MB         | loading plugins from C:\arnold\sdk\latest\bin\..\plugins ...
+00:00:00   116MB         |  alembic_proc.dll: alembic uses Arnold 7.1.1.0
+00:00:00   116MB         |  cryptomatte.dll: cryptomatte uses Arnold 7.1.1.0
+00:00:00   116MB         |  cryptomatte.dll: cryptomatte_filter uses Arnold 7.1.1.0
+00:00:00   116MB         |  cryptomatte.dll: cryptomatte_manifest_driver uses Arnold 7.1.1.0
+00:00:00   116MB         |  skipping already loaded plugin: C:\arnold\sdk\latest\bin\..\plugins\usd_proc.dll ...
+00:00:00   116MB         | loaded 4 plugins from 2 lib(s) in 0:00.00
+00:00:00   116MB         | [color_manager_ocio] default ocio.config found in C:\arnold\sdk\latest\bin\../ocio/configs/arnold/config.ocio
+00:00:00   132MB         | [kick] command: C:\arnold\sdk\latest\bin\kick test_resaved.ass -dw -r 160 120 -sm lambert -bs 16 -o testrender.tif -set driver_tiff.dither false -nocrashpopup -dp -v 6
+00:00:00   132MB         | loading plugins from . ...
+00:00:00   132MB         | no plugins loaded
+00:00:00   138MB         | [metadata] loading metadata file: test_resaved.ass
+00:00:00   139MB         | [ass] loading test_resaved.ass ...
+00:00:00   139MB         | [ass] read 8929 bytes, 11 nodes in 0:00.00
+00:00:00   139MB         | [kick] applying 2 attr value overrides
+00:00:00   139MB         | 
+00:00:00   139MB         | authorizing with default license managers: rlm, network, user ...
+00:00:06   145MB WARNING | rendering with watermarks because of failed authorization:
+00:00:06   145MB         |  [rlm] error initializing license system:
+00:00:06   145MB         |  [rlm]  * Can't read license data (-102)No such file or directory (errno: 2)
+00:00:06   145MB         |  [clm.v1] license server was not set (51)
+00:00:06   145MB         |  [clm.v2] SERVICE (14): entitlement expired
+00:00:06   145MB         |  environment variables:
+00:00:06   145MB         |         ARNOLD_LICENSE_ORDER   = (not set)
+00:00:06   145MB         |         ARNOLD_LICENSE_MANAGER = (not set)
+00:00:06   145MB         |  [rlm]  solidangle_LICENSE     = (not set)
+00:00:06   145MB         |  [rlm]  RLM_LICENSE            = (not set)
+00:00:06   145MB         |  [clm]  ADSKFLEX_LICENSE_FILE  = (not set)
+00:00:06   145MB         |  [clm]  LM_LICENSE_FILE        = (not set)
+00:00:06   145MB         | 
+00:00:06   145MB         | [color_manager] using color manager of type "color_manager_ocio"
+00:00:06   146MB         | [color_manager_ocio] using config file C:\arnold\sdk\latest\bin\../ocio/configs/arnold/config.ocio
+00:00:06   146MB         | [color_manager] rendering color space is "ACEScg"
+00:00:06   160MB         |  
+00:00:06   160MB         | there are 1 light and 3 objects:
+00:00:06   160MB         |       1 persp_camera
+00:00:06   160MB         |       1 mesh_light
+00:00:06   160MB         |       1 ray_switch_shader
+00:00:06   160MB         |       2 utility
+00:00:06   160MB         |       1 standard_surface
+00:00:06   160MB         |       1 multiply
+00:00:06   160MB         |       1 two_sided
+00:00:06   160MB         |       1 driver_tiff
+00:00:06   160MB         |       1 gaussian_filter
+00:00:06   160MB         |       2 polymesh
+00:00:06   160MB         |       2 list_aggregate
+00:00:06   160MB         |       2 color_manager_ocio
+00:00:06   160MB         |  
+00:00:06   160MB         | rendering image at 160 x 120, 3 AA samples
+00:00:06   160MB         |   AA samples max    <disabled>
+00:00:06   160MB         |   AA sample clamp   <disabled>
+00:00:06   160MB         |   diffuse           samples  2 / depth  1
+00:00:06   160MB         |   specular          samples  2 / depth  1
+00:00:06   160MB         |   transmission      samples  2 / depth  8
+00:00:06   160MB         |   volume indirect   <disabled by depth>
+00:00:06   160MB         |   total                          depth 10
+00:00:06   160MB         |   bssrdf            samples  2
+00:00:06   160MB         |   transparency      depth 10
+00:00:06   160MB         | initializing 14 nodes ...
+00:00:06   161MB         |  creating root object list ...
+00:00:06   161MB         | node initialization done in 0:00.00 (multithreaded)
+00:00:06   161MB         | updating 15 nodes ...
+00:00:06   161MB         |  /pCylinder1/light_pCylinder1/light_pCylinder1Shape/mesh/light: mesh_light using 1 sample, 2 volume samples
+00:00:06   161MB         |  scene bounds: (-13.4414387 -1.35595 -13.4414387) -> (13.4414387 1.24383485 13.4414387)
+00:00:06   161MB         | node update done in 0:00.00 (multithreaded)
+00:00:06   161MB         | [aov] parsing 1 output statements ...
+00:00:06   162MB         | [aov] registered driver: "defaultArnoldDriver/driver_exr.RGBA" (driver_tiff)
+00:00:06   162MB         | [aov]  * "RGBA" of type RGBA filtered by "defaultArnoldFilter/gaussian_filter" (gaussian_filter)
+00:00:06   162MB         | [aov] done preparing 2 AOVs for 1 output to 1 driver (0 deep AOVs)
+00:00:06   164MB         | starting 64 bucket workers of size 16x16 ...
+00:00:06   165MB         |  [accel] list_aggregate bvh4 done - 0:00.00 (wall time) - 2 prims, 1 key
+00:00:06   167MB         |  [accel] polymesh bvh4 done - 0:00.00 (wall time) - 60 prims, 1 key
+00:00:06   174MB         |  [accel] polymesh bvh4 done - 0:00.00 (wall time) - 100 prims, 1 key
+00:00:06   177MB         |    0% done - 26 rays/pixel
+00:00:06   211MB         |    5% done - 168 rays/pixel
+00:00:06   229MB         |   10% done - 225 rays/pixel
+00:00:06   243MB         |   15% done - 182 rays/pixel
+00:00:06   245MB         |   20% done - 59 rays/pixel
+00:00:06   249MB         |   25% done - 151 rays/pixel
+00:00:06   252MB         |   30% done - 128 rays/pixel
+00:00:06   255MB         |   35% done - 150 rays/pixel
+00:00:06   256MB         |   40% done - 89 rays/pixel
+00:00:06   257MB         |   45% done - 63 rays/pixel
+00:00:06   258MB         |   50% done - 79 rays/pixel
+00:00:06   259MB         |   55% done - 38 rays/pixel
+00:00:06   259MB         |   60% done - 28 rays/pixel
+00:00:06   260MB         |   65% done - 39 rays/pixel
+00:00:06   260MB         |   70% done - 32 rays/pixel
+00:00:06   261MB         |   75% done - 38 rays/pixel
+00:00:06   261MB         |   80% done - 24 rays/pixel
+00:00:06   262MB         |   85% done - 44 rays/pixel
+00:00:06   262MB         |   90% done - 25 rays/pixel
+00:00:06   262MB         |   95% done - 26 rays/pixel
+00:00:06   262MB         |  100% done - 3 rays/pixel
+00:00:06   262MB         | render done in 0:00.102
+00:00:06   262MB         | [driver_tiff] writing file `testrender.tif'
+00:00:06   264MB         | render done
+00:00:06   264MB         |  
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | scene creation time          0:00.24    machine utilization (0.10%) 
+00:00:06   264MB         |  unaccounted                 0:00.23 
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | frame time                   0:06.69    machine utilization (1.03%) 
+00:00:06   264MB         |  rendering                   0:00.10 
+00:00:06   264MB         |   pixel rendering            0:00.10 
+00:00:06   264MB         |  unaccounted                 0:06.58 
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | top session self-times by category 
+00:00:06   264MB         |  surface closure                                                    0:00.01 (21.15%) 
+00:00:06   264MB         |   aiStandardSurface1                                                0:00.01 (20.82%) 
+00:00:06   264MB         |  AiLightsTrace                                                      0:00.00 (11.12%) 
+00:00:06   264MB         |  TraceCameraRay                                                     0:00.00 (10.26%) 
+00:00:06   264MB         |  AiLightsPrepare (/pPlane1/pPlaneShape1)                            0:00.00 ( 8.03%) 
+00:00:06   264MB         |  standard_surface (aiStandardSurface1)                              0:00.00 ( 7.43%) 
+00:00:06   264MB         |  polymesh::intersect                                                0:00.00 ( 7.41%) 
+00:00:06   264MB         |   /pCylinder1/light_pCylinder1/light_pCylinder1Shape/mesh           0:00.00 ( 5.79%) 
+00:00:06   264MB         |   /pPlane1/pPlaneShape1                                             0:00.00 ( 1.62%) 
+00:00:06   264MB         |  BVH::intersect                                                     0:00.00 ( 6.41%) 
+00:00:06   264MB         |   root                                                              0:00.00 ( 3.84%) 
+00:00:06   264MB         |   /pCylinder1/light_pCylinder1/light_pCylinder1Shape/mesh           0:00.00 ( 1.94%) 
+00:00:06   264MB         |   /pPlane1/pPlaneShape1                                             0:00.00 ( 0.63%) 
+00:00:06   264MB         |  ray traversal+intersection                                         0:00.00 ( 6.09%) 
+00:00:06   264MB         |  sampleNextBatch                                                    0:00.00 ( 6.09%) 
+00:00:06   264MB         |  accumulateBucketSamples                                            0:00.00 ( 3.22%) 
+00:00:06   264MB         |  AiSceneLoad                                                        0:00.00 ( 2.21%) 
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | top session self-times by node 
+00:00:06   264MB         |  standard_surface:aiStandardSurface1                                0:00.03 (46.46%) 
+00:00:06   264MB         |   surface closure                                                   0:00.01 (20.82%) 
+00:00:06   264MB         |   AiLightsTrace                                                     0:00.00 (11.12%) 
+00:00:06   264MB         |   ray traversal+intersection                                        0:00.00 ( 4.82%) 
+00:00:06   264MB         |   AiMakeRay                                                         0:00.00 ( 1.62%) 
+00:00:06   264MB         |   AiShaderEvalParamFuncFlt                                          0:00.00 ( 0.65%) 
+00:00:06   264MB         |  polymesh:/pPlane1/pPlaneShape1                                     0:00.00 (12.22%) 
+00:00:06   264MB         |   AiLightsPrepare                                                   0:00.00 ( 8.03%) 
+00:00:06   264MB         |   polymesh::intersect                                               0:00.00 ( 1.62%) 
+00:00:06   264MB         |   ray traversal+intersection                                        0:00.00 ( 0.65%) 
+00:00:06   264MB         |   TraceShadow                                                       0:00.00 ( 0.65%) 
+00:00:06   264MB         |   BVH::intersect                                                    0:00.00 ( 0.63%) 
+00:00:06   264MB         |  TraceCameraRay                                                     0:00.00 (10.26%) 
+00:00:06   264MB         |  polymesh:/pCylinder1/light_pCylinder1/light_pCylinder1Shape/mesh     0:00.00 ( 7.73%) 
+00:00:06   264MB         |   polymesh::intersect                                               0:00.00 ( 5.79%) 
+00:00:06   264MB         |   BVH::intersect                                                    0:00.00 ( 1.94%) 
+00:00:06   264MB         |  sampleNextBatch                                                    0:00.00 ( 6.09%) 
+00:00:06   264MB         |  list_aggregate:root (BVH::intersect)                               0:00.00 ( 3.84%) 
+00:00:06   264MB         |  accumulateBucketSamples                                            0:00.00 ( 3.22%) 
+00:00:06   264MB         |  AiSceneLoad                                                        0:00.00 ( 2.21%) 
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | peak CPU memory used      264.14MB 
+00:00:06   264MB         |  at startup                94.80MB 
+00:00:06   264MB         |  AOV samples                2.89MB 
+00:00:06   264MB         |  output buffers             0.33MB 
+00:00:06   264MB         |  framebuffers               0.31MB 
+00:00:06   264MB         |  node overhead              0.00MB 
+00:00:06   264MB         |  message passing            0.13MB 
+00:00:06   264MB         |  memory pools              75.55MB 
+00:00:06   264MB         |  geometry                   0.01MB 
+00:00:06   264MB         |   polymesh                  0.01MB 
+00:00:06   264MB         |  accel structs              0.02MB 
+00:00:06   264MB         |  mesh importance map        0.01MB 
+00:00:06   264MB         |  strings                   24.25MB 
+00:00:06   264MB         |  profiler                   0.04MB 
+00:00:06   264MB         |  unaccounted               65.80MB 
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | ray counts                       ( /pixel, /sample) (% total) (avg. hits) (max hits)
+00:00:06   264MB         |  camera                   192800 (  10.04,    1.00) ( 12.56%) (     0.85) (       1)
+00:00:06   264MB         |  shadow                   199542 (  10.39,    1.03) ( 13.00%) (     0.01) (       1)
+00:00:06   264MB         |  diffuse_reflect          572192 (  29.80,    2.97) ( 37.27%) (     0.09) (       1)
+00:00:06   264MB         |  specular_reflect         570798 (  29.73,    2.96) ( 37.18%) (     0.11) (       1)
+00:00:06   264MB         |  total                   1535332 (  79.97,    7.96) (100.00%) (     0.18) (       1)
+00:00:06   264MB         | by ray depth:              0     1
+00:00:06   264MB         |  total                   25.6% 74.4% 
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | shader calls                     (  /pixel,  /sample) (% total)
+00:00:06   264MB         |  primary                  322200 (   16.78,     1.67) (100.00%)
+00:00:06   264MB         |  total                    322200 (   16.78,     1.67) (100.00%)
+00:00:06   264MB         | by ray depth:              0     1
+00:00:06   264MB         |  total                   64.5% 35.5% 
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | geometry                         (% hit ) (instances) (  init mem,  final mem)
+00:00:06   264MB         |  lists                         1 (100.0%) (        0) (      0.00,       0.00)
+00:00:06   264MB         |  polymeshes                    2 (100.0%) (        0) (      0.00,       0.01)
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | geometric elements               (      min) (       avg.) (      max)
+00:00:06   264MB         |  polygons                    160 (       60) (       80.0) (      100)
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | triangle tessellation            (      min) (       avg.) (      max) (/ element) (% total)
+00:00:06   264MB         |  polymeshes                  280 (       80) (      140.0) (      200) (     1.75) (100.00%)
+00:00:06   264MB         |  unique triangles            280 
+00:00:06   264MB         |  CPU memory use             0.01MB 
+00:00:06   264MB         |   vertices                  0.00MB 
+00:00:06   264MB         |   vertex indices            0.00MB 
+00:00:06   264MB         |   packed normals            0.00MB 
+00:00:06   264MB         |   normal indices            0.00MB 
+00:00:06   264MB         |   uv coords                 0.00MB 
+00:00:06   264MB         |   uv coords idxs            0.00MB 
+00:00:06   264MB         |   uniform indices           0.00MB 
+00:00:06   264MB         |   userdata                  0.00MB 
+00:00:06   264MB         |  largest polymeshes by triangle count 
+00:00:06   264MB         |        200 tris -- /pPlane1/pPlaneShape1
+00:00:06   264MB         |         80 tris -- /pCylinder1/light_pCylinder1/light_pCylinder1Shape/mesh
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | acceleration structures:         (% total)
+00:00:06   264MB         |  bvh                           3 (100.00%)
+00:00:06   264MB         |  total                         3 (100.00%)
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | number of warnings, warning type:
+00:00:06   264MB         |     1:  rendering with watermarks because of failed authorization:
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:06   264MB         | performance warnings:
+00:00:06   264MB WARNING |  Rendering CPU utilization was only 1%. Your render may be bound by a single threaded process or I/O.
+00:00:06   264MB         | -----------------------------------------------------------------------------------
+00:00:07   264MB         |  
+00:00:07   264MB         | releasing resources
+00:00:07   168MB         |   
+00:00:07   168MB         |  releasing resources
+00:00:07   147MB         |   unloading 3 plugins
+00:00:07   147MB         |    closing usd_proc.dll ...
+00:00:07   144MB         |    closing alembic_proc.dll ...
+00:00:07   144MB         |    closing cryptomatte.dll ...
+00:00:07   144MB         |   unloading plugins done
+00:00:07   144MB         |  Arnold shutdown

translator/reader/read_geometry.cpp
@@ -343,6 +343,19 @@ void UsdArnoldReadMesh::Read(const UsdPrim &prim, UsdArnoldReaderContext &contex
     // Check the prim visibility, set the AtNode visibility to 0 if it's hidden
     if (!context.GetPrimVisibility(prim, frame))
         AiNodeSetByte(node, str::visibility, 0);
+
+    // Check if there is a parameter primvars:arnold:light
+    UsdAttribute meshLightAttr = prim.GetAttribute(str::t_primvars_arnold_light);
+    bool meshLight = false;
+    if (meshLightAttr && meshLightAttr.Get(&meshLight, frame) && meshLight) {
+        // we have a geometry light for this mesh
+        std::string lightName = AiNodeGetName(node);
+        lightName += "/light";
+        AtNode *meshLightNode = context.CreateArnoldNode("mesh_light", lightName.c_str());
+        AiNodeSetPtr(meshLightNode, str::mesh, (void*)node);
+        // Read the arnold parameters for this light
+        ReadArnoldParameters(prim, context, meshLightNode, time, "primvars:arnold:light");
+    }
 }
 
 class CurvesPrimvarsRemapper : public PrimvarsRemapper

commit e19023ebf6665bbef7fa3ea1391d6cda0371c193

Fix build issue in the render delegate introduced by #1147

render_delegate/mesh.cpp
@@ -361,9 +361,9 @@ void HdArnoldMesh::Sync(
                 // If we have a mesh light, we want to check for light attributes 
                 // with a "light:" namespace
                 if (meshLight) {
-                    // ignore the attribute arnold:light:enable which is just meant
+                    // ignore the attribute arnold:light which is just meant
                     // to trigger the creation of the mesh light
-                    if (primvar.first == _tokens->arnold_light)
+                    if (primvar.first == str::t_arnold_light)
                         continue;
                     std::string primvarStr = primvar.first.GetText();
                     const static std::string s_lightPrefix = "arnold:light:";
@@ -488,7 +488,7 @@ bool HdArnoldMesh::_IsVolume() const { return AiNodeGetFlt(GetArnoldNode(), str:
 AtNode *HdArnoldMesh::_GetMeshLight(HdSceneDelegate* sceneDelegate, const SdfPath& id)
 {
     bool hasMeshLight = false;
-    VtValue lightValue = sceneDelegate->Get(id, _tokens->arnold_light);
+    VtValue lightValue = sceneDelegate->Get(id, str::t_arnold_light);
     if (lightValue.IsHolding<bool>()) {
         hasMeshLight = lightValue.UncheckedGet<bool>();            
     }