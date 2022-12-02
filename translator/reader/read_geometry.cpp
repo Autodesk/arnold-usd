7cc7b26e9d4448866719dbad66dbfa1e3be6cb3d
Sebastien Blaineau-Ortega <sebastien.blaineau.ortega@autodesk.com>
2022-12-02 10:47:23 +0100

    Skip curves without any width (#1363)
    * Skip curves with empty radius #1334
    
    * Don't evaluate widths parameter twice #1334

--- translator/reader/read_geometry.cpp
+++ translator/reader/read_geometry.cpp
@@ -434,6 +434,14 @@ void UsdArnoldReadCurves::Read(const UsdPrim &prim, UsdArnoldReaderContext &cont
     TimeSettings staticTime(time);
     staticTime.motionBlur = false;
 
+    UsdGeomCurves curves(prim);
+
+    VtValue widthValues;
+    if (!curves.GetWidthsAttr().Get(&widthValues, frame)) {
+        AiMsgWarning("[usd] Skipping curves with empty width %s", prim.GetPath().GetText());
+        return;
+    }
+ 
     AtNode *node = context.CreateArnoldNode("curves", prim.GetPath().GetText());
 
     AtString basis = str::linear;
@@ -462,9 +470,11 @@ void UsdArnoldReadCurves::Read(const UsdPrim &prim, UsdArnoldReaderContext &cont
 #endif
         }
     }
+
+
+
     AiNodeSetStr(node, str::basis, basis);
 
-    UsdGeomCurves curves(prim);
     // CV counts per curve
     ReadArray<int, unsigned int>(curves.GetCurveVertexCountsAttr(), node, "num_points", staticTime);
 
@@ -473,24 +483,21 @@ void UsdArnoldReadCurves::Read(const UsdPrim &prim, UsdArnoldReaderContext &cont
 
     // Widths
     // We need to divide the width by 2 in order to get the radius for arnold points
-    VtValue widthValues;
     VtIntArray vertexCounts;
     curves.GetCurveVertexCountsAttr().Get(&vertexCounts, frame);
     const auto vstep = basis == str::bezier ? 3 : 1;
     const auto vmin = basis == str::linear ? 2 : 4;
     ArnoldUsdCurvesData curvesData(vmin, vstep, vertexCounts);
     
-    if (curves.GetWidthsAttr().Get(&widthValues, frame)) {
-        TfToken widthInterpolation = curves.GetWidthsInterpolation();
-        if ((widthInterpolation == UsdGeomTokens->vertex || widthInterpolation == UsdGeomTokens->varying) &&
-                basis != str::linear) {
-            // if radius data is per-vertex and the curve is pinned, then don't remap
-            if (!(widthInterpolation == UsdGeomTokens->vertex && isValidPinnedCurve))
-                curvesData.RemapCurvesVertexPrimvar<float, double>(widthValues);
-            curvesData.SetRadiusFromValue(node, widthValues);
-        } else {
-            curvesData.SetRadiusFromValue(node, widthValues);
-        }
+    TfToken widthInterpolation = curves.GetWidthsInterpolation();
+    if ((widthInterpolation == UsdGeomTokens->vertex || widthInterpolation == UsdGeomTokens->varying) &&
+            basis != str::linear) {
+        // if radius data is per-vertex and the curve is pinned, then don't remap
+        if (!(widthInterpolation == UsdGeomTokens->vertex && isValidPinnedCurve))
+            curvesData.RemapCurvesVertexPrimvar<float, double>(widthValues);
+        curvesData.SetRadiusFromValue(node, widthValues);
+    } else {
+        curvesData.SetRadiusFromValue(node, widthValues);
     }
 
     ReadMatrix(prim, node, time, context);
