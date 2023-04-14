a7fa11596cffc08825bc2c97819a3de3cb28e99c
Sebastien Blaineau-Ortega <sebastien.blaineau.ortega@autodesk.com>
2023-04-14 18:14:36 +0200

    Cherry picks to the fix-7.2.1 branch (#1504)
    * Ensure shader scope doesn't appear twice in the hierarchy (#1462)
    
    * Ensure hierarchy doesn't show twice in the surface shader names #1457
    
    * Simplifying test scene #1457
    
    * Adding comments #1457
    
    * Ensure we don't get duplicated scopes on the whole shading tree #1457
    
    (cherry picked from commit cf2d96845d693f0def7dc9c13d575fc1be175a9b)
    
    * change colorspace such that there is no conversion happening (#1474)
    
    (cherry picked from commit d57293763ef9e3acd8983212d4b599b34caf695b)
    
    * Apply skinned transforms to the mesh points (#1464)
    
    * use the skinned xforms on points.
    
    * remove unused deformationFlags variable
    
    * change adapters vectors to a smart pointers
    
    * remove ArnoldUsdSkelBakeSkinningParms
    
    * remove unused prim member
    
    * remove unused _HashComparePrim
    
    * remove unused GetSizeEstimate
    
    * remove unused bindings argument
    
    * apply the skinned xform to the normals as well
    
    (cherry picked from commit 8eaeff53c27a6c6e690cc418a4207565804338a9)
    
    * Support multiple shader outputs (#1465)
    
    * Fix connections on shaders with multiple outputs (proc) #1459
    
    * Support multiple shader outputs in the render delegate #1459
    
    (cherry picked from commit f12713810c0f584f51ff9b74b9c19b8e8211dddf)
    
    * update changelog with the most recent merges (#1478)
    
    (cherry picked from commit a02ecfbd3e54b65462286733869b1b6ef2e2b4bd)
    
    * Refresh the arnold instancer when the prototype mesh has changed (#1489)
    
    * Add missing info to changelog (#1490)
    
    (cherry picked from commit 39e24ae582c8370aa80cc1d9198557e921fc924d)
    
    * MaterialX shader nodes should have "auto" colorspace by default (#1494)
    
    * MaterialX shader nodes should have "auto" colorspace by default #1485
    
    * Read materialx colorSpace in the procedural #1485
    
    * Support colorspace in the procedural #1485
    
    (cherry picked from commit 1aad4940362116e053b64a0cd53ae39d7321d594)
    
    * Update tests and changelog (#1497)
    
    * Update tests and changelog
    
    * Update test scene for 1459
    
    (cherry picked from commit 2858a8c9354a93bf5defd4b58ebf0152d1e36b3a)
    
    * handle normals when the primvar is indexed and has a vertex interpolation (#1484)
    
    ---------
    
    Co-authored-by: cpichard <cpichard@users.noreply.github.com>

---
+++ testsuite/test_1457/data/test.cpp
@@ -0,0 +1,36 @@
+#include <ai.h>
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+int main(int argc, char **argv)
+{
+    AiBegin(); 
+    AiMsgSetConsoleFlags(nullptr, AI_LOG_ALL);
+    AtParamValueMap* params = AiParamValueMap();
+    AiSceneLoad(nullptr, "scene.ass", nullptr);
+    AiSceneWrite(nullptr, "scene.usda", params);
+    AiParamValueMapDestroy(params);
+    AiEnd();
+
+    AiBegin();
+    AiSceneLoad(nullptr, "scene.usda", nullptr);
+
+    const static AtString shaderName("/test/aiStandardSurface1/aiStandardSurface1");
+    AtNode *shader = AiNodeLookUpByName(nullptr, shaderName);
+    const static AtString noiseName("/test/aiStandardSurface1/my_noise");
+    AtNode *noise = AiNodeLookUpByName(nullptr, noiseName);
+    bool success = (shader != nullptr && noise != nullptr);
+    AiEnd();
+
+    return (success) ? 0 : 1;
+}
+
+
+
+
+
+
+
--- translator/reader/read_skinning.h
+++ translator/reader/read_skinning.h
@@ -35,59 +35,6 @@ PXR_NAMESPACE_USING_DIRECTIVE
 
 class UsdArnoldReaderContext;
 
-/// Parameters for configuring ArnoldUsdSkelBakeSkinning.
-struct ArnoldUsdSkelBakeSkinningParms
-{
-    /// Flags for identifying different deformation paths.
-    enum DeformationFlags {
-        DeformPointsWithLBS = 1 << 0,
-        DeformNormalsWithLBS = 1 << 1,
-        DeformXformWithLBS = 1 << 2,
-        DeformPointsWithBlendShapes = 1 << 3,
-        DeformNormalsWithBlendShapes = 1 << 4,
-        DeformWithLBS = (DeformPointsWithLBS|
-                         DeformNormalsWithLBS|
-                         DeformXformWithLBS),
-        DeformWithBlendShapes = (DeformPointsWithBlendShapes|
-                                 DeformNormalsWithBlendShapes),
-        DeformAll = DeformWithLBS|DeformWithBlendShapes,
-        /// Flags indicating which components of skinned prims may be
-        /// modified, based on the active deformations.
-        ModifiesPoints = DeformPointsWithLBS|DeformPointsWithBlendShapes,
-        ModifiesNormals = DeformNormalsWithLBS|DeformNormalsWithBlendShapes,
-        ModifiesXform = DeformXformWithLBS
-    };
-
-    /// Flags determining which deformation paths are enabled.
-    int deformationFlags = DeformAll;
-    
-    /// Memory limit for pending stage writes, given in bytes.
-    /// If zero, memory limits are ignored. Otherwise, output stages
-    /// are flushed each time pending writes exceed this amount.
-    /// Note that at least one frame of data for *all* skinned prims
-    /// will be held in memory prior to values being written to disk,
-    /// regardless of this memory limit.    
-    /// Since flushing pending changes requires layers to be saved,
-    /// memory limiting is only active when _saveLayers_ is enabled.
-    size_t memoryLimit = 0;
-
-    /// If true, extents of UsdGeomPointBased-derived prims are updated
-    /// as new skinned values are produced. This is made optional
-    /// in case an alternate procedure is being used to compute
-    /// extents elsewhere.
-    bool updateExtents = true;
-
-    /// If true, extents hints of models that already stored
-    /// an extentsHint are updated to reflect skinning changes.
-    /// All extent hints are authored to the stage's current edit target.
-    bool updateExtentHints = true;
-
-    /// The set of bindings to bake.
-    std::vector<UsdSkelBinding> bindings;
-    
-};
-
-
 struct UsdArnoldSkelDataImpl;
 
 class UsdArnoldSkelData {
