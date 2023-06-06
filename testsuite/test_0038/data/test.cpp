7fefd644093c90369c49b2dd389cb3f14a915e6d
Sebastien Blaineau-Ortega <sebastien.blaineau.ortega@autodesk.com>
2023-06-06 20:14:28 +0200

    Update tests related to viewport API (#1555)

--- testsuite/test_0038/data/test.cpp
+++ testsuite/test_0038/data/test.cpp
@@ -10,21 +10,25 @@ int main(int argc, char **argv)
     AiBegin();
 
     AtUniverse *proc_universe = AiUniverse();
+    AtUniverse *render_universe = AiUniverse();
+    AtRenderSession *render_session = AiRenderSession(render_universe);
 
-    AiSceneLoad(nullptr, "scene.ass", nullptr);
+    AiSceneLoad(render_universe, "scene.ass", nullptr);
     // load the usd procedural (containing a sphere) in a separate universe
-    AiSceneLoad(proc_universe, "usd_proc.ass", nullptr);
     AtNode *proc_a = AiNode(proc_universe, "usd", "usd_proc_a");
     AiNodeSetStr(proc_a, "filename", "usd_proc_a.usda");
     // Call the viewport API on the usd procedural, and fill the main universe
-    AiProceduralViewport(proc_a, nullptr, AI_PROC_POLYGONS);
+    AiProceduralViewport(proc_a, render_universe, AI_PROC_POLYGONS);
     AtNode *proc_b = AiNode(proc_universe, "usd", "usd_proc_b");
     AiNodeSetStr(proc_b, "filename", "usd_proc_b.usda");
     // Call the viewport API on the usd procedural, and fill the main universe
-    AiProceduralViewport(proc_b, nullptr, AI_PROC_BOXES);
+    AiProceduralViewport(proc_b, render_universe, AI_PROC_BOXES);
 
     AiUniverseDestroy(proc_universe);
-    AiRender(nullptr);
+    AiRender(render_session);
+
+    AiRenderSessionDestroy(render_session);
+    AiUniverseDestroy(render_universe);
 
     AiEnd();
     return 0;
