5b2950566c44d46769296407fc67e368e8a1245d
Sebastien Blaineau-Ortega <sebastien.blaineau.ortega@autodesk.com>
2023-10-30 07:34:36 +0100

    Add support for -j argument in husk #1077 (#1736)
    * Add support for -j argument in husk #1077
    
    * Add changelog
    
    * Skip command line token for expected values

--- libs/render_delegate/render_delegate.cpp
+++ libs/render_delegate/render_delegate.cpp
@@ -649,9 +649,17 @@ void HdArnoldRenderDelegate::_SetRenderSetting(const TfToken& _key, const VtValu
         if (value.IsHolding<VtStringArray>()) {
             const VtStringArray commandLine = value.UncheckedGet<VtArray<std::string>>();
             for (unsigned int i = 0; i < commandLine.size(); ++i) {
+                // husk argument for output image
                 if (commandLine[i] == "-o" && i < commandLine.size() - 2) {
-                    _outputOverride = commandLine[i+1];
+                    _outputOverride = commandLine[++i];
                     break;
+                }
+                // husk argument for thread count (#1077)
+                if ((commandLine[i] == "-j" || commandLine[i] == "--threads") 
+                        && i < commandLine.size() - 2) {
+                    // if for some reason the argument value is not a number, atoi should return 0
+                    // which is also the default arnold value. 
+                    AiNodeSetInt(_options, str::threads, std::atoi(commandLine[++i].c_str()));
                 }
             }
         }
