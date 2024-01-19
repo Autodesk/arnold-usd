53821034eb00a03b3754944fd13ced369f3c7c80
Cyril Pichard <cyril.pichard@autodesk.com>
2024-01-19 09:05:29 +0000

    compatibility with older arnold versions

--- libs/render_delegate/render_delegate.cpp
+++ libs/render_delegate/render_delegate.cpp
@@ -351,7 +351,7 @@ void _RemoveArnoldGlobalPrefix(const TfToken& key, TfToken& key_new)
 
 } // namespace
 
-#if ARNOLD_VERSION_NUM >= 70104
+#if ARNOLD_VERSION_NUM > 70203
 const AtNodeEntry * HdArnoldRenderDelegate::GetMtlxCachedNodeEntry(const std::string &nodeEntryKey, const AtString &nodeType, AtParamValueMap *params) {
     // First we check if the nodeType is an arnold shader
     std::lock_guard<AtMutex> lock(_nodeEntrymutex);
@@ -365,7 +365,8 @@ const AtNodeEntry * HdArnoldRenderDelegate::GetMtlxCachedNodeEntry(const std::st
     }
     return shaderNodeEntryIt->second;
 };
-
+#endif
+#if ARNOLD_VERSION_NUM >= 70104
 AtString HdArnoldRenderDelegate::GetCachedOslCode(const std::string &oslCodeKey, const AtString &nodeType, AtParamValueMap *params) {
     std::lock_guard<AtMutex> lock(_oslCodeCacheMutex);
     const auto oslCodeIt = _oslCodeCache.find(oslCodeKey);
