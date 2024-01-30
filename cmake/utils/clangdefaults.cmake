6d1de7a85bee30699bfe50ad1edaef1ad993cbb6
Cyril Pichard <cyril.pichard@autodesk.com>
2024-01-30 18:52:36 +0000

    remove deprecated linker warning

--- cmake/utils/clangdefaults.cmake
+++ cmake/utils/clangdefaults.cmake
@@ -1,6 +1,4 @@
 
-string(APPEND CMAKE_SHARED_LINKER_FLAGS " -Wl,-undefined,error")
-
 add_compile_options($<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>:-Wno-deprecated>)
 
 add_compile_definitions(_LIBCPP_ENABLE_CXX17_REMOVED_UNARY_BINARY_FUNCTION)
