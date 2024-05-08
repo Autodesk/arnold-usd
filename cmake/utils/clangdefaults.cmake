c098e80a9d4e6642eb0d72cfa4f20e9367479289
Cyril Pichard <cyril.pichard@autodesk.com>
2024-05-08 17:34:30 +0100

    fix cmake warning regarding cmake_minimum_version

--- cmake/utils/clangdefaults.cmake
+++ cmake/utils/clangdefaults.cmake
@@ -1,4 +1,5 @@
 
-add_compile_options($<$<OR:$<CXX_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:Clang>>:-Wno-deprecated>)
-
 add_compile_definitions(_LIBCPP_ENABLE_CXX17_REMOVED_UNARY_BINARY_FUNCTION)
+add_compile_options(-Wno-deprecated)
+add_compile_options(-Wno-deprecated-declarations)
+add_compile_options(-Wno-deprecated-builtins)
