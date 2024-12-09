6539e4b1b463c90ff65492086c7bb07b600ab11a
Cyril Pichard <cyril.pichard@autodesk.com>
2024-12-09 14:44:03 +0000

    test failing in procedural mode

--- testsuite/test_0000.1/data/test.cpp
+++ testsuite/test_0000.1/data/test.cpp
@@ -3,7 +3,7 @@
 #include <cstdlib>
 int main(int argc, char **argv)
 {
-	if (std::getenv("PROCEDURAL_USE_HYDRA")!=nullptr) {
+	if (std::getenv("PROCEDURAL_USE_HYDRA")==nullptr) {
 		return 1;
 	}
 	return 0;
