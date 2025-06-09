98360f0458c599721fcb8d3abe84906555276f3b
Cyril Pichard <cyril.pichard@autodesk.com>
2025-06-09 14:53:58 +0100

    fix material pruning happening in hydra2 25.05

---
+++ libs/render_delegate/fixMaterialPruningSIP.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "pxr/pxr.h"
+
+#if PXR_VERSION >= 2505
+
+#include "pxr/imaging/hd/sceneIndexPlugin.h"
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+/// \class HdArnoldFixMaterialPruningSceneIndexPlugin
+/// 
+/// This scene index filter re-enable the materials that are type erased in the PiPrototypePropagatingSceneIndex.
+/// This problem should normally be fixed in 25.08 
+///
+
+class HdArnoldFixMaterialPruningSceneIndexPlugin : public HdSceneIndexPlugin
+{
+public:
+    HdArnoldFixMaterialPruningSceneIndexPlugin();
+
+protected:
+    HdSceneIndexBaseRefPtr _AppendSceneIndex(
+        const HdSceneIndexBaseRefPtr &inputScene,
+        const HdContainerDataSourceHandle &inputArgs) override;
+};
+
+PXR_NAMESPACE_CLOSE_SCOPE
+
+#endif
