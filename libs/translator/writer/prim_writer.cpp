9501ab2c78b32ae176b766cd2bfbe703d688c421
Cyril Pichard <cyril.pichard@autodesk.com>
2025-09-24 15:58:21 +0100

    rename AI_USERDEF_SCATTER by AI_USERDEF_PER_INSTANCE

--- libs/translator/writer/prim_writer.cpp
+++ libs/translator/writer/prim_writer.cpp
@@ -418,7 +418,7 @@ public:
                 category = UsdGeomTokens->faceVarying;
                 break;
 #ifdef USE_NATIVE_INSTANCING
-            case AI_USERDEF_SCATTER:
+            case AI_USERDEF_PER_INSTANCE:
                 category = TfToken("instance"); // TODO have a static token
                 break;
 #endif
